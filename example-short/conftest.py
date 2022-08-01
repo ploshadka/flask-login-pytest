87191ccfec0d4ac24564b47a7eb90fc91ae56ff6
Ilya K <32592936+ploshadka@users.noreply.github.com>
2022-08-01 15:18:29 +0300

    Update conftest.py

--- example-short/conftest.py
+++ example-short/conftest.py
@@ -7,7 +7,7 @@ from app.models.users import User
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
-    # Обходит @login_required
+    # Bypasses @login_required
     app.config["LOGIN_DISABLED"] = True
     with app.test_client() as client:
         yield client
@@ -15,9 +15,9 @@ def client():
 
 @pytest.fixture
 def authenticated_request():
-    # Решает проблему с flask_login.current_user.id:
+    # Solves the problem with flask_login.current_user.id:
     # AttributeError: 'AnonymousUser' object has no attribute 'id'
-    # Создает авторизацию пользователя
+    # Creates a user authorization
     with app.test_request_context():
         test_user = db.session.query(User).filter_by(id=1).first()
         yield flask_login.login_user(test_user)
